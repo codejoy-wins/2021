e62742217c493714ce29c52199cc48d202da0cad
Max <mjann@ucsc.edu>
2021-02-04 22:50:39 -0800

    animations

---
+++ contact.js
@@ -0,0 +1,8 @@
+function reveal(){
+    document.getElementById("secret").innerHTML=`8059405096`;
+    console.log("revealed");
+}
+function reveal2(){
+    document.getElementById("secret2").innerHTML=`maxwellljann@gmail.com`;
+    console.log("revealed2");
+}
