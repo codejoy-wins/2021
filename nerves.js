49b93359eba8c39d0e6a06e6dd3967b5d63a2dfd
Max <mjann@ucsc.edu>
2021-02-21 01:39:07 -0800

    blog style

---
+++ nerves.js
@@ -0,0 +1,10 @@
+function cocktail(){
+    document.getElementById("cocktail_comments").classList.remove("hidden");
+    document.getElementById("shower").classList.add("hidden");
+    document.getElementById("hider").classList.remove("hidden");
+}
+function cocktail2(){
+    document.getElementById("cocktail_comments").classList.add("hidden");
+    document.getElementById("shower").classList.remove("hidden");
+    document.getElementById("hider").classList.add("hidden");
+}
