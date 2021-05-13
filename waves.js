ceed7fbce09cd85ca8e84c8da24940cf0d0c5023
Max <mjann@ucsc.edu>
2021-05-13 00:51:30 -0700

    danger zone

--- waves.js
+++ waves.js
@@ -7,7 +7,7 @@ let zeus = new Date();
 let horse = zeus.getHours();
 console.log(horse);
 
-if(horse>18 && horse >7){
+if(horse>18 || horse <7){
     console.log("night mode");
     night();
 }else{
@@ -17,12 +17,12 @@ if(horse>18 && horse >7){
 function night(){
     document.getElementById("time").innerHTML=`Good Evening - ${zeus}`;
     document.getElementById("xp").innerHTML=`
-    <video autoplay="autoplay" loop class="vid" controls src="static/img/ocean.mp4" type="video/mp4"></video>
+    <video autoplay="autoplay" loop class="vid" controls src="static/img/codejoy-wins.mp4" type="video/mp4"></video>
     `
 }
 function day(){
     document.getElementById("time").innerHTML=`Good Morning - ${zeus}`;
     document.getElementById("xp").innerHTML=`
-    <video autoplay="autoplay" loop class="vid" controls src="static/img/ocean2.mp4" type="video/mp4"></video>
+    <video autoplay="autoplay" loop class="vid" controls src="static/img/killjoy-wins.mp4" type="video/mp4"></video>
     `
 }
