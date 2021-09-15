16481893871d6fe6104dce8710b03a9122d8fdaf
Max <mjann@ucsc.edu>
2021-09-15 00:03:12 -0700

    evil

--- logos.js
+++ logos.js
@@ -1,9 +1,18 @@
 const maxwell = ()=>{
     const burger = document.querySelector('.burger');
+    const closer = document.querySelector('.perfect');
+
+
     const navs = document.querySelector('.nav-links');
 
     burger.addEventListener('click', ()=>{
         console.log("ahhh");
+        navs.classList.toggle('super');
+        burger.classList.toggle('super');
+
+    })
+    closer.addEventListener('click', ()=>{
+        console.log("closing nav");
         navs.classList.toggle('super');
         burger.classList.toggle('super');
 
