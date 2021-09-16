cbdc1aabdd185e0feef3a78b837aa0ad252a93ec
Max <mjann@ucsc.edu>
2021-09-15 20:13:46 -0700

    bigvids

--- logos.js
+++ logos.js
@@ -1,9 +1,12 @@
 const maxwell = ()=>{
     const burger = document.querySelector('.burger');
     const closer = document.querySelector('.perfect');
-
-
+    const mega = document.querySelector('.megaphone');
+    const song = document.querySelector('.song');
     const navs = document.querySelector('.nav-links');
+    const vid = document.querySelector('.backyard');
+
+    let isplaying = true;
 
     burger.addEventListener('click', ()=>{
         console.log("ahhh");
@@ -16,6 +19,24 @@ const maxwell = ()=>{
         navs.classList.toggle('super');
         burger.classList.toggle('super');
 
+    })
+
+    vid.addEventListener('click',()=>{
+        console.log("background clicked");
+        console.log(vid);
+        vid.play();
+    })
+
+    mega.addEventListener('click', ()=>{
+        if(isplaying==false){
+            console.log("playing now")
+            song.play();
+            isplaying = true;
+        }else{
+            console.log("volume toggle");
+            song.pause();
+            isplaying = false;
+        }
     })
 }
 
