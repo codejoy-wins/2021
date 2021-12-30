95b3a884bb19848cbf35b512906dbdff564d7c01
Max <mjann@ucsc.edu>
2021-12-29 21:08:13 -0800

    ahhh

---
+++ hey.js
@@ -0,0 +1,103 @@
+// if user is on phone inject text " i see you're on your phone"
+// if user is not on phone inject text "thank you for using your computer"
+
+function crazy(x){
+    console.log(`crazy of ${x}`);
+    if(x<3){
+        console.log("x is less than 3");
+    }else{
+        console.log("x isn't less than 3");
+    }
+}
+crazy(32);
+crazy(1);
+
+let xyz = document.getElementById("background-video");
+
+const maxwell = ()=>{
+    const burger = document.querySelector('.burger');
+    const navs = document.querySelector('.nav-links');
+    
+
+    burger.addEventListener('click', ()=>{
+        console.log("ahhh");
+        navs.classList.toggle('super');
+        burger.classList.toggle('green');
+
+    })
+
+    
+}
+
+maxwell();
+
+const deviceType = () => {
+    const ua = navigator.userAgent;
+    if (/(tablet|ipad|playbook|silk)|(android(?!.*mobi))/i.test(ua)) {
+        console.log('tab');
+        return "tablet";
+    }
+    else if (/Mobile|Android|iP(hone|od)|IEMobile|BlackBerry|Kindle|Silk-Accelerated|(hpw|web)OS|Opera M(obi|ini)/.test(ua)) {
+        console.log('mobile');
+        return "mobile";
+    }
+    console.log("desktop");
+    return "desktop";
+};
+deviceType();
+
+const helloWorld = () => {
+    if(deviceType()=="desktop"){
+        console.log("I see you're on a desktop");
+        document.getElementById("intro").innerHTML=`
+            <p>
+            Welcome, I see you're on your desktop.
+            </p>
+            <p>
+            That provides the best user experience
+            </p>
+        `
+    }
+    if(deviceType()=="tablet"){
+        console.log("I see you're on a tablet");
+        document.getElementById("intro").innerHTML=`
+            <p>
+            Welcome, I see you're on your tablet.
+            </p>
+        `
+    }
+    if(deviceType()=="mobile"){
+        console.log("I see you're on your phone");
+        document.getElementById("intro").innerHTML=`
+            <p>
+            Welcome, I see you're on your phone.
+            </p>
+            <p>
+            The nav bar for you is those three little lines at the top
+            </p>
+        `
+    }
+    
+}
+helloWorld();
+
+const nextTime = () =>{
+    let mytiming = new Date();
+    let correcttime = mytiming.getHours();
+    let pikachu = mytiming.getMinutes();
+    let day = mytiming.getDay();
+
+    if(correcttime>12){
+        correcttime = correcttime -12;
+    }
+    console.log('time');
+    document.getElementById("next").innerHTML=`
+    <p>
+        Is it ${correcttime} o'clock already?
+    </p>
+    <p>
+        Only ${5-day} more days til the weekend
+    </p>
+    `
+}
+nextTime();
