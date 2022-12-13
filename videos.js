db17f7217779bf4e4483fa535e38c3b755cb8e8e
Max <mjann@ucsc.edu>
2022-12-13 08:40:02 -0800

    happy jesus

--- videos.js
+++ videos.js
@@ -5,6 +5,13 @@ function alls(){
     `
     document.getElementById("article").innerHTML=`
 
+    <div class="video">
+        <h1>Jesus Dream</h1>
+        <div class="vid2">
+        <iframe width="100%" height="315" src="https://www.youtube.com/embed/TYWSzlpNbHk" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+        </div>
+    </div>
+
     <div class="video2">
     <h1>Church of Elam</h1>
     <div class="vid2">
@@ -17,7 +24,6 @@ function alls(){
     <iframe width="100%" height="315" src="https://www.youtube.com/embed/-sYl-uZ79p8" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
     </div>
 
-    </div>
     <div class="video">
         <h1>Gothic Chess</h1>
         <div class="vid2">
@@ -572,6 +578,13 @@ function conspiracies(){
     <h1> Philosophy </h1>
     `
     document.getElementById("article").innerHTML=`
+
+    <div class="video">
+        <h1>Jesus Dream</h1>
+        <div class="vid2">
+        <iframe width="100%" height="315" src="https://www.youtube.com/embed/TYWSzlpNbHk" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+        </div>
+    </div>
 
     <div class="video">
     <h1>Church of Elam</h1>
