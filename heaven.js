a1da8241f2e2bc427b9e85930fce39f63456b06c
Max <mjann@ucsc.edu>
2023-05-07 14:57:40 -0700

    what's the password?

--- heaven.js
+++ heaven.js
@@ -1,5 +1,22 @@
 function cloudland(){
     console.log("welcome to cloudland");
-    // e.preventDefault();
+    let k = document.getElementById("passwordx").value;
+    console.log(k);
+    event.preventDefault();
+    if(k=="elam" || k=="Elam"){
+        gates();
+    }
 }
-// how to stop form from refreshing page??
+
+function gates(){
+    console.log("gating");
+    document.getElementById("show").innerHTML=`
+    <img class="nude" src="static/img/heaven.jpg" alt="heaven">
+    <p>
+        I look at this picture and all I can think of is Heaven.  They say St. Peter is at the gates of heaven, and that the path is narrow.
+    </p>
+    <p>
+        Could this be a reference to a tight vagina as the gates to heaven?  Peter meaning penis?
+    </p>
+    `
+}
