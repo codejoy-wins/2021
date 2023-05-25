d23963b8e8ea56a639261da94c4ec2bc1a61d6fe
Max <mjann@ucsc.edu>
2023-05-25 15:32:44 -0700

    contact restructuring

--- contact.js
+++ contact.js
@@ -1,8 +1,4 @@
-function reveal(){
-    document.getElementById("secret").innerHTML=`8059405096`;
-    console.log("revealed");
+function elam(){
+    console.log("Elam in progress");
 }
-function reveal2(){
-    document.getElementById("secret2").innerHTML=`maxwellljann@gmail.com`;
-    console.log("revealed2");
-}
+elam();
