7ae52e1ab9fc0b2f6a0ccc5c67937b2378ac5096
Max <mjann@ucsc.edu>
2023-05-25 21:27:41 -0700

    biz idea

---
+++ business.js
@@ -0,0 +1,186 @@
+// geting canvas by Boujjou Achraf
+var c = document.getElementById("c");
+var ctx = c.getContext("2d");
+// xp
+//toDataURL() canvas function almost works to make canvas a background
+// document.getElementById("new3").style.background = "url(" + canvas.toDataURL() + ")";
+
+//making the canvas full screen
+// c.height = window.innerHeight;
+// c.width = window.innerWidth;
+// new height try
+let maxheight;
+if(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)){
+    // true for mobile device
+    console.log("mobile device");
+    maxheight=window.innerHeight*1.411;
+  }else{
+    // false for not mobile device
+    console.log("not mobile device");
+    maxheight=window.innerHeight*1.117;
+  }
+c.height = maxheight;
+c.width = window.innerWidth;
+//mobile
+
+
+//chinese characters - taken from the unicode charset
+var matrix = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ123456789@#$%^&*()*&^%+-/~{[|`]}";
+var matrix2 = "elamahplacloudlandwhitebloodcellELAMAHPLACLOUDLANDWHITEBLOODCELL117$$$117}";
+
+//converting the string into an array of single characters
+matrix2 = matrix2.split("");
+
+var font_size = 10;
+var columns = c.width/font_size; //number of columns for the rain
+//an array of drops - one per column
+var drops = [];
+//x below is the x coordinate
+//1 = y co-ordinate of the drop(same for every drop initially)
+for(var x = 0; x < columns; x++)
+    drops[x] = 1; 
+
+//drawing the characters
+function draw()
+{
+    //Black BG for the canvas
+    //translucent BG to show trail
+    // ctx.fillStyle = "rgba(0, 0, 0, 0.04)";
+    ctx.fillStyle = "rgba(0, 0, 0, 0.117)";
+
+    ctx.fillRect(0, 0, c.width, c.height);
+
+    // ctx.fillStyle = "#f4427d";//green text
+    ctx.fillStyle = "gold";//green text
+
+    ctx.font = font_size + "px arial";
+    //looping over drops
+    for(var i = 0; i < drops.length; i++)
+    {
+        //a random chinese character to print
+        var text = matrix2[Math.floor(Math.random()*matrix2.length)];
+        //x = i*font_size, y = value of drops[i]*font_size
+        ctx.fillText(text, i*font_size, drops[i]*font_size);
+
+        //sending the drop back to the top randomly after it has crossed the screen
+        //adding a randomness to the reset to make the drops scattered on the Y axis
+        if(drops[i]*font_size > c.height && Math.random() > 0.975)
+            drops[i] = 0;
+
+        //incrementing Y coordinate
+        drops[i]++;
+    }
+}
+
+setInterval(draw, 35);
+
+
+
+
+let xyz = document.getElementById("background-video");
+
+const maxwell = ()=>{
+    const burger = document.querySelector('.burger');
+    const navs = document.querySelector('.nav-links');
+    // 'click'
+
+    burger.addEventListener('click', ()=>{
+        console.log("ahhh");
+        navs.classList.toggle('super');
+        burger.classList.toggle('green');
+
+    })
+    
+}
+
+maxwell();
+
+
+
+const nextTime = () =>{
+    let mytiming = new Date();
+    let correcttime = mytiming.getHours();
+    let pikachu = mytiming.getMinutes();
+    let correctday = mytiming.getDay();
+    let day = correctday;
+    let mandela = false;
+    let weekend = false;
+    if(day>=5){
+        weekend = true;
+    }
+    if(day==0){
+        weekend = true;
+    }
+
+    if(correcttime>12){
+        correcttime = correcttime -12;
+    }
+    if(correcttime == 0){
+        correcttime = 12;
+    }
+    console.log('time');
+    if(!weekend){
+        document.getElementById("next").innerHTML=`
+        <p>
+            Is it ${correcttime} o'clock already?
+        </p>
+        <p>
+            Only ${5-day} more days til the weekend
+        </p>
+        `
+    }
+    else{
+        document.getElementById("next").innerHTML=`
+        <p>
+            Is it ${correcttime} o'clock already?
+        </p>
+        <p>Enjoy your weekend</p>
+    `
+    }
+    
+}
+
+let songz = [
+    `hates`,
+    `Lying`,
+    `cali`,
+    `Afterlie`,
+    `diamond`,
+    `vice`,
+    `AdoreYou`,
+    `only`,
+    `winter`,
+    `herestoyou`,
+    `fakebook`,
+]
+
+let songs = [
+       `This Town Hates Me`,
+       `Lying`,
+       `California`,
+       `Afterli(f)e`,
+       `Cold Diamond`,
+       `Add Vice for a Friend`,
+       `Adore You`,
+       `If Only`,
+       `Beautiful Winter`,
+       `Here's to You`,
+       `Fakebook`,
+]
+
+function randosong(){
+    let x = Math.random(0,songz.length);
+    x=x*10;
+    x = Math.floor(x);
+    
+    console.log(`x is ${x}`);
+    document.getElementById("introsong").innerHTML=`
+    <audio id="player" onended="randosong()" src="static/audio/${songz[x]}.mp3"></audio>
+    `
+    document.getElementById("descsong").innerHTML=`
+    <p>${songs[x]}</p>
+    `
+}
+
+nextTime();
+randosong();
